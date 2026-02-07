9ef016e5bd8d97255b64d05129d1f5700b8d4fd5
Arya Phunne <aryaphunne2005@gmail.com>
2026-02-07 13:33:42 +0530

    Build ready for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? `/venus-nextjs` : "";
-
 const nextConfig = {
   // output: "export", // Commented out to enable API routes for NextAuth
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
 };
 
 export default nextConfig;
